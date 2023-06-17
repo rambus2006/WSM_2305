f3248dcf06b18f1ef82e862dcdea7d4557bee14e
School_rambus06 <w2227@e-mirim.hs.kr>
2023-06-17 21:12:27 +0900

    프로젝트

--- 수행평가/FitBook/JavaScript/nav.js
+++ 수행평가/FitBook/JavaScript/nav.js
@@ -1,14 +1,14 @@
-const navToggleI = nav_toggle.getElementsByTagName("i")[0];
-const navListUl = document.getElementsByClassName("nav-list")[0];
+const navbar_links = nav_toggle.getElementsByTagName("i")[0];
+const navbar_menu = document.getElementsByClassName("nav_item")[0];
 
-console.log(navToggleI);
-console.log(navListUl);
+console.log(navbar_links);
+console.log(navbar_menu);
 //.nav-toggle클릭하면
 nav_toggle.onclick=()=>{
     //.nav-toggle i 햄버거메뉴 <-> X
     //클래스에서 bi-list <-> bi-x-lg
-    navToggleI.classList.toggle("bi-list");
-    navToggleI.classList.toggle("bi-x-lg");
+    navbar_links.classList.toggle("bi-list");
+    navbar_links.classList.toggle("bi-x-lg");
     //.nav-list .show-menu toggle
-    navListUl.classList.toggle("show-menu");
+    navbar_menu.classList.toggle("show-menu");
 };
