8a0964a03e895ac8e0d24f4f396e382a6a779b09
School_rambus06 <w2227@e-mirim.hs.kr>
2023-06-20 14:57:36 +0900

    nav css 디테일의 차이를 만들자

--- 수행평가/FitBook/JavaScript/nav.js
+++ 수행평가/FitBook/JavaScript/nav.js
@@ -1,5 +1,5 @@
 const navbar_links = nav_toggle.getElementsByTagName("i")[0];
-const navbar_menu = document.getElementsByClassName("nav_item")[0];
+const navbar_menu = document.getElementsByClassName("nav_item");
 
 console.log(navbar_links);
 console.log(navbar_menu);
@@ -10,5 +10,5 @@ nav_toggle.onclick=()=>{
     navbar_links.classList.toggle("bi-list");
     navbar_links.classList.toggle("bi-x-lg");
     //.nav-list .show-menu toggle
-    navbar_menu.classList.toggle("show-menu");
+    navbar_links.classList.toggle("show-menu");
 };
