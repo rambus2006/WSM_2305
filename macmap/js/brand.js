e19d1746ecb9a5ae0c2b3fa71aa6958756526a01
rambus06 <w2227@e-mirim.hs.kr>
2023-11-11 23:36:34 +0900

    macmap

--- macmap/js/brand.js
+++ macmap/js/brand.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 
 
 let divfromhtml = document.getElementsByClassName('postcontents');
@@ -7,3 +8,14 @@ let post = document.createElement('div');
 post.setAttribute('class','post');
 
 divfromhtml.appendChild(post);
+=======
+
+
+let divfromhtml = document.getElementsByClassName('postcontents');
+
+let post = document.createElement('div');
+
+post.setAttribute('class','post');
+
+divfromhtml.appendChild(post);
+>>>>>>> f1d49be0baeee418e7d0a378a819cb85dcc301dc
--- macmap/js/main.js
+++ macmap/js/main.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 const toggleBtn = document.querySelector('.navbar__toggleBtn');
 const menu = document.querySelector('.navbar__menu');
 const links = document.querySelector('.navbar__links');
@@ -5,4 +6,13 @@ const links = document.querySelector('.navbar__links');
 toggleBtn.addEventListener('click', () => {
     menu.classList.toggle('active');
     links.classList.toggle('active');
+=======
+const toggleBtn = document.querySelector('.navbar__toggleBtn');
+const menu = document.querySelector('.navbar__menu');
+const links = document.querySelector('.navbar__links');
+
+toggleBtn.addEventListener('click', () => {
+    menu.classList.toggle('active');
+    links.classList.toggle('active');
+>>>>>>> f1d49be0baeee418e7d0a378a819cb85dcc301dc
 })
