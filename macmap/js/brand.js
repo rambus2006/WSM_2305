36293003680d5341c39836bd17b2e113e4f1d5c9
rambus06 <w2227@e-mirim.hs.kr>
2023-11-14 15:08:07 +0900

    Add files via upload

---
+++ macmap/js/brand.js
@@ -0,0 +1,21 @@
+<<<<<<< HEAD
+
+
+let divfromhtml = document.getElementsByClassName('postcontents');
+
+let post = document.createElement('div');
+
+post.setAttribute('class','post');
+
+divfromhtml.appendChild(post);
+=======
+
+
+let divfromhtml = document.getElementsByClassName('postcontents');
+
+let post = document.createElement('div');
+
+post.setAttribute('class','post');
+
+divfromhtml.appendChild(post);
+>>>>>>> f1d49be0baeee418e7d0a378a819cb85dcc301dc
